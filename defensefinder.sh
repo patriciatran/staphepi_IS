199b82f909587c5c1443480b23ec6bbf9a11feb8
Patricia Tran <ptran5@wisc.edu>
2025-06-18 14:59:33 -0500

    Finding defense and anti-defense systems in the genomes

--- defensefinder.sh
+++ defensefinder.sh
@@ -5,6 +5,8 @@ SAMPLE="$1"
 hmmsearch -h
 defense-finder run -h
 
-defense-finder run -o defense-finder_${SAMPLE} --models-dir /projects/bacteriology_tran_data/defense_finder_v2.0.0_models/ -a protein.faa
+defense-finder run -o defense-finder_${SAMPLE} --log-level DEBUG --models-dir /projects/bacteriology_tran_data/defense_finder_v2.0.0_models/ -a protein.faa
+
+ls -lht defense-finder_${SAMPLE}
 
 tar -cvf defense-finder_${SAMPLE}.tar.gz defense-finder_${SAMPLE}
