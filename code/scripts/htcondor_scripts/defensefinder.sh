64c73bbc3a2c194fde6d3bbdc845da47e1903072
patriciatran <ptran5@wisc.edu>
2026-02-02 12:45:48 -0600

    updated chtc scripts

--- code/scripts/htcondor_scripts/defensefinder.sh
+++ code/scripts/htcondor_scripts/defensefinder.sh
@@ -5,7 +5,11 @@ SAMPLE="$1"
 hmmsearch -h
 defense-finder run -h
 
-defense-finder run -o defense-finder_${SAMPLE} --log-level DEBUG --models-dir /projects/bacteriology_tran_data/defense_finder_v2.0.0_models/ -a protein.faa
+defense-finder run \
+	-o defense-finder_${SAMPLE} \
+	--log-level DEBUG \
+	--models-dir /projects/bacteriology_tran_data/defense_finder_v2.0.0_models/ \
+	-a ${SAMPLE}.fasta
 
 ls -lht defense-finder_${SAMPLE}
 
